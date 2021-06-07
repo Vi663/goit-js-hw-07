ce53bd8d193524c81fe02c3ab4e7983ab1a681c0
Вікторія Юрченко <viktoriau26@gmail.com>
2021-06-08 02:02:26 +0300

    +

--- js/task-08.js
+++ js/task-08.js
@@ -1,18 +1,37 @@
 const mainContainer = document.getElementById('controls');
+const divContainer = document.getElementById('boxes');
 const inputEl = document.querySelector("[type='number']");
 const renderButton = document.querySelector("[data-action='render']");
 const destroyButton = document.querySelector("[data-action='destroy']");
 
-// renderButton.addEventListener('click', onButtonRender);
 // destroyButton.addEventListener('click', onButtonDestroy);
+let quantity = 5;
+// const amount = inputEl.currentTarget.value;
+function onInputChange(event) {
+  const amount = event.currentTarget.value;
+  console.log(`Amount is ${amount}`);
+  quantity === amount;
+};
 
 inputEl.addEventListener('input', onInputChange);
-function onInputChange(event) {
-  let amount = event.target.value;
-  return amount;
+console.log(quantity);
+function createBoxes(amount){
+  
+  renderButton.addEventListener('click', function () {
+    for (let i; i === quantity; i += 1) {
+      const newEl = document.createElement('div');
+      newEl.classList.add('container');
+      newEl.style.backgroundColor = red;
+      newEl.width = 30;
+      mainContainer.append(newEl);
+    }
+  
+  });
 };
-console.log(mainContainer.input);
-console.log(`Amount is ${amount}`);
+
+
+
+
 
 // function createBoxes(amount) {
 //   onButtonRender(event) {
