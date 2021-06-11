df0d5fd508d7fb6788e0b3ba9a0835a755a27867
Вікторія Юрченко <viktoriau26@gmail.com>
2021-06-11 21:57:18 +0300

    +

--- js/task-08.js
+++ js/task-08.js
@@ -5,33 +5,23 @@ const renderButton = document.querySelector("[data-action='render']");
 const destroyButton = document.querySelector("[data-action='destroy']");
 
 // destroyButton.addEventListener('click', onButtonDestroy);
-let quantity = 5;
-// const amount = inputEl.currentTarget.value;
-function onInputChange(event) {
-  const amount = event.currentTarget.value;
-  console.log(`Amount is ${amount}`);
-  quantity === amount;
-};
+// let quantity = 0;
+// function onInputChange(event) {
+//   quantity = event.currentTarget.value;
+// };
+// renderButton.addEventListener('click', onInputChange);
+// inputEl.addEventListener('input', onInputChange);
+// // console.log(`Amount is ${quantity}`);
 
-inputEl.addEventListener('input', onInputChange);
-console.log(quantity);
-function createBoxes(amount){
-  
-  renderButton.addEventListener('click', function () {
-    for (let i; i === quantity; i += 1) {
-      const newEl = document.createElement('div');
-      newEl.classList.add('container');
-      newEl.style.backgroundColor = red;
-      newEl.width = 30;
-      mainContainer.append(newEl);
-    }
-  
-  });
-};
-
-
-
-
+// function createBoxes(event) {
+//   forEach (let i; i <= quantity; i += 1) {
+//     const newEl = document.createElement('div');
+//     newEl.classList.add('container');
+//     newEl.style.backgroundColor = red;
+//     newEl.style.width = 30;
+//     divContainer.appendChild(newEl);
+//   };
+// };
 
 // function createBoxes(amount) {
 //   onButtonRender(event) {
@@ -42,4 +32,3 @@ function createBoxes(amount){
 //   };
   
 // };
-
